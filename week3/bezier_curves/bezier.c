81d90ea699c2e682c0273c6e616d145286d11891
Tom Clement <mr@tomclement.nl>
2016-02-17 14:47:37 +0100

    Added bezier curve eval + drawing

--- week3/bezier_curves/bezier.c
+++ week3/bezier_curves/bezier.c
@@ -18,6 +18,24 @@
 #include "bezier.h"
 #include <stdio.h>
 
+long factorial(int n) {
+    long result = 1;
+
+    for (int c = 1; c <= n; c++) {
+        result = result * c;
+    }
+
+    return result;
+}
+
+double binomial_distribution(int n, int k) {
+    return factorial(n) / (double)(factorial(k) * factorial(n - k));
+}
+
+double bernstein_polynomial(int n, int i, float u) {
+    return binomial_distribution(n, i) * pow(u, (double)i) * pow((1 - u), (double)(n-i));
+}
+
 /* Given a Bezier curve defined by the 'num_points' control points
  * in 'p' compute the position of the point on the curve for parameter
  * value 'u'.
@@ -27,10 +45,16 @@
  */
 
 void
-evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u)
-{
+evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u) {
     *x = 0.0;
     *y = 0.0;
+    double bernstein = 0.0;
+
+    for(int i = 0; i < num_points; i++) {
+        bernstein = bernstein_polynomial(num_points-1, i, u);
+        *x += bernstein * p[i].x;
+        *y += bernstein * p[i].y;
+    }
 }
 
 /* Draw a Bezier curve defined by the control points in p[], which
@@ -55,8 +79,16 @@ evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, flo
  */
 
 void
-draw_bezier_curve(int num_segments, control_point p[], int num_points)
-{
+draw_bezier_curve(int num_segments, control_point p[], int num_points) {
+    float x, y;
+    double stepSize = 1.0 / num_points;
+
+    glBegin(GL_LINE_STRIP);
+    for(double u = 0; u <= 1.001; u += 0.05) {
+        evaluate_bezier_curve(&x, &y, p, num_points, u);
+        glVertex2f(x, y);
+    }
+    glEnd();
 }
 
 /* Find the intersection of a cubic Bezier curve with the line X=x.
@@ -66,8 +98,7 @@ draw_bezier_curve(int num_segments, control_point p[], int num_points)
 */
 
 int
-intersect_cubic_bezier_curve(float *y, control_point p[], float x)
-{
+intersect_cubic_bezier_curve(float *y, control_point p[], float x) {
     return 0;
 }
 
